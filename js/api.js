053736f3dd6fd0864e45a29276e0d6b8387d04ff
Sanjoy1210 <sanjoy.paul@hotmail.com>
2021-09-01 23:14:07 +0600

    catch the input value by clicking button

--- js/api.js
+++ js/api.js
@@ -1,1 +1,18 @@
-console.log('api.js file connected');
+const searchButton = document.getElementById('search-btn');
+
+const fetchedData = async url => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
+
+searchButton.addEventListener('click', () => {
+  const inputField = document.getElementById('input-field');
+  const searchText = inputField.value;
+  inputField.value = '';
+
+  // console.log(inputValue);
+  const url = `http://openlibrary.org/search.json?q=${searchText}`;
+  fetchedData(url)
+    .then(books => console.log(books.docs));
+});
